c03f14383bea9c0cd59d6809eeac0af7959ac561
ZachsFinalForm <51968089+ZachsFinalForm@users.noreply.github.com>
2019-06-23 23:33:34 -0400

    Add files via upload

---
+++ Baseball.py
@@ -0,0 +1,226 @@
+#reads multiple files
+import Utilities as u
+
+AllStarFull = u.readABaseBallCSV('AllstarFull.csv')
+
+for i in AllStarFull:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Appearances = u.readABaseBallCSV('Appearances.csv')
+
+for i in Appearances:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+AwardsManagers = u.readABaseBallCSV('AwardsManagers.csv')
+
+for i in AwardsManagers:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+AwardsPlayers = u.readABaseBallCSV('AwardsPlayers.csv')
+
+for i in AwardsPlayers:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+AwardsShareManagers = u.readABaseBallCSV('AwardsShareManagers.csv')
+
+for i in AwardsShareManagers:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+AwardsSharePlayers = u.readABaseBallCSV('AwardsSharePlayers.csv')
+
+for i in AwardsSharePlayers:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Batting = u.readABaseBallCSV('Batting.csv')
+
+for i in Batting:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+BattingPost = u.readABaseBallCSV('BattingPost.csv')
+
+for i in BattingPost:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+CollegePlaying = u.readABaseBallCSV('CollegePlaying.csv')
+
+for i in CollegePlaying:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Fielding = u.readABaseBallCSV('Fielding.csv')
+
+for i in Fielding:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+FieldingOF = u.readABaseBallCSV('FieldingOF.csv')
+
+for i in FieldingOF:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Appearances = u.readABaseBallCSV('Appearances.csv')
+
+for i in Appearances:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+FieldingOFsplit = u.readABaseBallCSV('FieldingOFsplit.csv')
+
+for i in FieldingOFsplit:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+FieldingPost = u.readABaseBallCSV('FieldingPost.csv')
+
+for i in FieldingPost:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+HallOfFame = u.readABaseBallCSV('HallOfFame.csv')
+
+for i in HallOfFame:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+HomeGames = u.readABaseBallCSV('HomeGames.csv')
+
+for i in HomeGames:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Managers = u.readABaseBallCSV('Managers.csv')
+
+for i in Managers:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+ManagersHalf = u.readABaseBallCSV('ManagersHalf.csv')
+
+for i in ManagersHalf:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Parks = u.readABaseBallCSV('Parks.csv')
+
+for i in Parks:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+People = u.readABaseBallCSV('People.csv')
+
+for i in People:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Pitching = u.readABaseBallCSV('Pitching.csv')
+
+for i in Pitching:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+PitchingPost = u.readABaseBallCSV('PitchingPost.csv')
+
+for i in PitchingPost:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Salaries = u.readABaseBallCSV('Salaries.csv')
+
+for i in Salaries:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Schools = u.readABaseBallCSV('Schools.csv')
+
+for i in Schools:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+SeriesPost = u.readABaseBallCSV('SeriesPost.csv')
+
+for i in SeriesPost:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+Teams = u.readABaseBallCSV('Teams.csv')
+
+for i in Teams:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+TeamsFranchises = u.readABaseBallCSV('TeamsFranchises.csv')
+
+for i in TeamsFranchises:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
+
+TeamsHalf = u.readABaseBallCSV('TeamsHalf.csv')
+
+for i in TeamsHalf:
+    x = i.split(',')
+    print(x[0])
+
+input("Hit any key to proceed")
